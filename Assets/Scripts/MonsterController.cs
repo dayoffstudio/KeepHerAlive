62f4be4e52a1d9967025dd47035f4c3c99047180
FayeKe <kyqq990304@163.com>
2020-05-14 15:37:11 +0800

    修改了怪物脚本

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -9,12 +9,31 @@ public class MonsterController : MonoBehaviour
 
     private Animator anim;
     private new Rigidbody2D rigidbody;
-    private bool monsterAwake = false;
+    //private bool monsterAwake = false;
     private bool dead = false;
     private bool attacked = false;
+
+    public float chaseRadius;    //追击半径，玩家进入后怪物会追击玩家
+    public float wanderRadius;   //放弃半径，当怪物超出会放弃追击
+    public float attackRange;    //攻击距离
+
     public float chaseSpeed;
     public float maxSpeed = 4;
     public float force = 8;
+    private enum MonsterState
+    {
+        WANDER,     //移动
+        CHASE,      //追击玩家
+        ATTACK      //攻击玩家
+    }
+    private MonsterState currentState = MonsterState.WANDER;//默认状态为移动
+    public float actRestTme;            //更换待机指令的间隔时间
+    private float lastActTime;          //最近一次指令时间
+    private int attackKind;             //攻击方式
+
+    private float diatanceToPlayer;         //怪物与玩家的距离
+    private Quaternion targetRotation;         //怪物的目标朝向
+
     public float getAttackForce = 2;
     public GameObject bulletHole;
     // Start is called before the first frame update
@@ -22,51 +41,82 @@ public class MonsterController : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
-    }
 
+        //检查并修正怪物设置
+        //攻击距离不大于自卫半径，否则就无法触发追击状态，直接开始战斗了
+        attackRange = Mathf.Min(chaseRadius, attackRange);
+        //游走半径不大于追击半径，否则怪物可能刚刚开始追击就停止
+        wanderRadius = Mathf.Min(chaseRadius, wanderRadius);
+    }
     // Update is called once per frame
     void Update()
     {
-        FindHuman();
-        anim.SetBool("dead",dead);
-        //追逐
-        if (monsterAwake)
+        switch (currentState)
         {
-            //agent.SetDestination(player.transform.position);
+            //游走，根据状态随机时生成的目标位置修改朝向，并向前移动
+            case MonsterState.WANDER:
+                //transform.Translate(Vector2.forward * Time.deltaTime * maxSpeed);
+                //转向指令
+                //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed);
+                //切换动画
+                anim.SetBool("chasing", false);
+                //该状态下的检测指令
+                WanderRadiusCheck();
+                //WanderRadiusCheck();
+                break;
+
+            //追击状态，朝着玩家跑去
+            case MonsterState.CHASE:
+                //切换动画
+                anim.SetBool("chasing", true);
+                //该状态下的检测指令
+                ChaseRadiusCheck();
+                break;
+            case MonsterState.ATTACK:
+                //切换动画
+                anim.SetBool("attack", true);
+                //攻击方式
+                Random rd = new Random();
+                //attackKind = rd.Next(1,2);
+                //anim.SetInteger("attackKind", attackKind);
+                anim.SetInteger("attackKind", 1);
+                break;
         }
     }
-    //寻找人类
-    void FindHuman()
-    {
-        if (!dead)
-        {
-            //得到与主角的距离                    
-            float distance = Vector2.Distance(this.transform.position, player.transform.position);
-            //距离判断         
-            if (distance > 8f)
-            {
-                monsterAwake = false;
-            }
-            else
-            {
-                monsterAwake = true;
-            }
-            if (!attacked)
-            {
-                anim.SetBool("chasing", monsterAwake);
-            }
 
-            
-            if (monsterAwake && distance <= 2.3f)
-            {
-                //判断攻击CD       
-                //攻击状态
-                //anim.SetBool("attack", true);
-                //anim.SetInteger("attackKind", 1);
-                //攻击动作结束在计时器中结束攻击状态
-            }
-        }    
+    ///
+    /// 游走状态检测，检测敌人距离
+    ///
+    void WanderRadiusCheck()
+    {
+        diatanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+        if (diatanceToPlayer < chaseRadius)
+        {
+            currentState = MonsterState.CHASE;
+        }
     }
+
+    ///
+    /// 追击状态检测，检测敌人是否进入攻击范围
+    ///
+    void ChaseRadiusCheck()
+    {
+        diatanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
+        //如果进入攻击距离就开始攻击
+        if (diatanceToPlayer < attackRange)
+        {
+            //检测攻击CD
+            //if() break;
+            currentState = MonsterState.ATTACK;
+        }
+        //如果超出追击范围则开始游走
+        if (diatanceToPlayer > wanderRadius)
+        {
+            currentState = MonsterState.WANDER;
+        }
+    }
+
+    //被攻击
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "Bullet")
@@ -95,6 +145,13 @@ public class MonsterController : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
+    }
+
+    //动画事件
+    void AfterAttack()
+    {
+        anim.SetInteger("attackKind", 0);
+        anim.SetBool("attack", false);
     }
     void AfterAttacked()
     {
